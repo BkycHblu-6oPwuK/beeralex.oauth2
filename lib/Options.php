9411ab993dda8ba62e533ca851ff549fd4ba8f74
Alexandr.Belotsitsko <sanya_belyy_0202@mail.ru>
2025-11-28 08:29:47 +0600

    Добавление функции логирования и класса Options для улучшения конфигурации модуля

---
+++ lib/Options.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Beeralex\Oauth2;
+
+use Beeralex\Core\Config\AbstractOptions;
+
+final class Options extends AbstractOptions
+{
+    protected function mapOptions(array $options): void
+    {
+        
+    }
+
+    public function getModuleId(): string
+    {
+        return 'beeralex.oauth2';
+    }
+}
